6f73cbb8b652e5175313d04fbc0087aaa4c5829d
temevh <temehama@gmail.com>
2024-01-23 12:16:04 +0200

    first somewhat working version

--- Ass1/ass1.py
+++ Ass1/ass1.py
@@ -6,15 +6,38 @@ import sys
 
 ## Implement your algorithm here:
 #For testpair1, v = 0, w = 7
+#g = testcase1.txt, B = 2 4 6
+
+#Start from first pair (v) 
+#check adjacency list 
+#if list contains a element that is in B 
+  #-> go to that element, increment n by one
+#else if no B element found 
+  #-> go to 
 def algorithm(g, B, v, w):
-  n = 0
-  #print("worked")
+    n = 0
+    visited = set()
+    node = v
+    print("Starting from node:", node)
+    print("Set B:", B)
 
+    while True:
+        adjacent = g.adj[node]
+        print("Adjacent vertices of", node, ":", adjacent)
 
+        # Check if any adjacent node is in set B
+        common_nodes = set(adjacent) & B
+        if common_nodes:
+            # Move to the first node in set B
+            next_node = common_nodes.pop()
+            print(f"Moving to node {next_node} from set B")
+            node = next_node
+            n += 1
+        else:
+            break  # No adjacent node in set B
 
-
-
-  return n
+    print("Total nodes from set B on the path:", n)
+    return n
 
 
 ### Read in a set of vertices from a file. These are just numbers separated by whitespace.
